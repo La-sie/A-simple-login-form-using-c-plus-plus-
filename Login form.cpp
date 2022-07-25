3ca486f538286b7f0b6b4fe27340893539cb9b15
Sellasie <fsdjogatse@st.ug.edu.gh>
2022-07-25 07:48:45 +0000

    Create Login form.cpp

---
+++ Login form.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int password = sella2367;
+    bool isTrue; 
+    
+    cout << " ***Welcome to ReinaCode***"
+         << "1.LOGIN"<< endl;
+         << "2.REGISTER"<< endl;
+         << "3.FORGOT PASSWORD <<endl;
+         << "4.USERNAME" << endl;
+         << "5.EXIT" << endl;
+         
+     cout << "Choose an option" << endl;
+     
+     if (pin == password ) {
+         
+     }
+     
+    return 0;
+}
